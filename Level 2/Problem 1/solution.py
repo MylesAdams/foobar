74dc594fae3a9af703cb4b1b6e115a70239f4d29
Myles Adams <myles.n.adams@gmail.com>
2018-04-04 12:43:13 -0700

    Clean up of Level 2 - Problem 1

--- Level 2/Problem 1/solution.py
+++ Level 2/Problem 1/solution.py
@@ -1,19 +1,3 @@
-# def answer(h, q):
-#     p = []
-#     tree = [None] * (2**h - 1)
-#     build_tree(tree, 1, 2**h-1, 0)
-#     print(tree)
-#
-#     for x in q:
-#         parent_index = (tree.index(x) - 1)//2
-#         if parent_index >= 0:
-#             p.append(tree[parent_index])
-#         else:
-#             p.append(-1)
-#
-#     return p
-
-
 def answer(h, q):
     p = []
     num = 2**h - 1
@@ -24,22 +8,18 @@ def answer(h, q):
     return p
 
 
-def build_tree(tree, low, num_values, index):
-    tree[index] = low + num_values - 1
-    if num_values != 1:
-        build_tree(tree, low, num_values//2, 2*index + 1)
-        build_tree(tree, low + num_values//2, num_values//2, 2*index + 2)
-
-
 def find_parent(value, high, num_values):
+    # Check if at root of current subtree
     if (high == value):
         return -1
 
+    # Check if in right subtree, else left subtree
     if (value >= high - num_values//2):
         parent = find_parent(value, high - 1, num_values // 2)
     else:
         parent = find_parent(value, high - num_values//2 - 1, num_values//2)
 
+    # Return label if label was returned, else return current root
     return parent if (parent != -1) else high
 
 
